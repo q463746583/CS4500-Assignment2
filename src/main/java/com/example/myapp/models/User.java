98009c57b1efdbc62499da3eab0e1174148ec6aa
cvan cao <cvanzy@cvans-MacBook-Air.local>
2019-01-22 13:18:16 -0500

    Add initialize object in User class

--- src/main/java/com/example/myapp/models/User.java
+++ src/main/java/com/example/myapp/models/User.java
@@ -46,4 +46,15 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    // Initialize object
+    public User() { }
+
+    public User(Integer id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
 }
