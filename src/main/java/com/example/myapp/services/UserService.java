f8c185aa33ab94a40513497c4b479189cfc0cdcd
cvan cao <cvanzy@cvans-MacBook-Air.local>
2019-01-22 13:22:59 -0500

    implement the function in the UserService

--- src/main/java/com/example/myapp/services/UserService.java
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -1,3 +1,5 @@
+package com.example.myapp.services;
+
 import com.example.myapp.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,21 +11,26 @@ import java.util.List;
 @RestController
 public class UserService {
     static List<User> users = new ArrayList();
+
     static {
         users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
         users.add(new User(234, "bob", "bob", "Bob", "Marley"));
     }
+
     @GetMapping("/api/user")
     public List<User> findAllUsers() {
         return users;
     }
+
     @GetMapping("/api/user/{userId}")
     public User findUserById(
             @PathVariable("userId") Integer id) {
-        for(User user: users) {
-            if(user.getId() == id) {
+        for (User user : users) {
+            if (user.getId() == id) {
                 return user;
             }
             return null;
         }
+        return null;
     }
+}
