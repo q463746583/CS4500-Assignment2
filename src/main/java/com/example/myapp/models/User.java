b8ad6d84d5cb21c15664da2c25702a432c5e906e
cvan cao <cvanzy@cvans-MacBook-Air.local>
2019-01-22 13:13:11 -0500

    Add all set and get function in User class

--- src/main/java/com/example/myapp/models/User.java
+++ src/main/java/com/example/myapp/models/User.java
@@ -9,10 +9,41 @@ public class User {
     // provide a public set and get method for each of the class variables above
     // make sure to use proper naming conventions for Java methods, e.g.,
     // methods should be camelcase. Here's an example using the firstName variable:
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getFirstName() {
         return firstName;
     }
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/com/example/myapp/services/UserService.java
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 public class UserService {
-    static Ligst<User> users = new ArrayList();
+    static List<User> users = new ArrayList();
     static {
         users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
         users.add(new User(234, "bob", "bob", "Bob", "Marley"));
